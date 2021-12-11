55457a60fb8caea82d7e4e63cff6276f331d5e7c
sp2728 <sp2728@njit.edu>
2021-12-11 17:49:20 -0500

    fixed npm build error for aws

--- src/components/Authentication/Register.js
+++ src/components/Authentication/Register.js
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { register } from '../../store/utils/thunkCreators';
 import { useDispatch } from 'react-redux';
 import FormGroup from '@mui/material/FormGroup';
-import {validateName, validateEmail} from '../Common/validation';
 
 export const Register = () => {
     const dispatch = useDispatch();
@@ -22,6 +21,46 @@ export const Register = () => {
         if (username && password && fullName && email && !errorStatus.error && retype && retype===password) {
             dispatch(register({username, password, fullName, email}));
         }
+    }
+
+    const validateName = (nameString)=>{
+        /**
+         * Validation function for Name in Form
+         * NOTE: Element with id as in `messageBoxId`, must be set to `{visibility: hidden}` style before running this function.
+         * @function: validateName
+         * @param {string} nameString - string for name value which is required to validate
+         * @returns {Object} {
+         *   error: true if there is an error else false (incase of no errors in validation).
+         *   message: indicating why the error has occured
+         * }
+         */
+        nameString = nameString.trim();
+        const errorStatus = {error: false, message: ""}, namePattern = /^[a-zA-Z\s0-9]+$/, maxLength = 128;
+        if(!namePattern.test(nameString) || nameString.length > maxLength){
+            errorStatus.message = `Name must only contain (0-9), (A-Z), (a-z) or spaces with max ${maxLength} characters.`;
+            errorStatus.error = true;
+        }
+        return errorStatus;
+    }
+    
+    const validateEmail = (nameString)=>{
+        /**
+         * Validation function for Name in Form
+         * NOTE: Element with id as in `messageBoxId`, must be set to `{visibility: hidden}` style before running this function.
+         * @function: validateName
+         * @param {string} nameString - string for name value which is required to validate
+         * @returns {Object} {
+         *   error: true if there is an error else false (incase of no errors in validation).
+         *   message: indicating why the error has occured
+         * }
+         */
+        nameString = nameString.trim();
+        const errorStatus = {error: false, message: ""}, namePattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, maxLength = 128;
+        if(!namePattern.test(nameString) || nameString.length > maxLength){
+            errorStatus.message = `Enter a valid email!`;
+            errorStatus.error = true;
+        }
+        return errorStatus;
     }
 
     return (
