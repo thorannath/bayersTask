d3edc1a47e4e17202d23d9648122c41c1259a68f
sp2728 <sp2728@njit.edu>
2022-01-31 01:49:30 -0500

    partially completed the sankey diagram

--- src/components/Charts/SankeyDiagram.js
+++ src/components/Charts/SankeyDiagram.js
@@ -1,10 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './Charts.css';
 import * as d3 from 'd3';
-import { select } from 'd3';
+import { select, interpolateCool, scaleSequential } from 'd3';
 import { sankey as d3Sankey, sankeyLinkHorizontal } from "d3-sankey";
 
 
+const SankeyNode = ({ name, x0, x1, y0, y1, color }) => (
+  <rect x={x0} y={y0} width={x1 - x0} height={y1 - y0} fill={color}>
+    <title>{name}</title>
+    <text x={x0+10} y={y0+50} width={x1 - x0} height={y1 - y0} fill='black'>{name}</text>
+  </rect>
+  
+);
+
+const SankeyLink = ({ link, color }) => (
+  <path
+    d={sankeyLinkHorizontal()(link)}
+    style={{
+      fill: "none",
+      strokeOpacity: ".3",
+      stroke: color,
+      strokeWidth: Math.max(1, link.width)
+    }}
+  />
+);
 const SankeyDiagram = ({ data }) => {
 
   const wrapperRef = useRef();
@@ -15,46 +34,45 @@ const SankeyDiagram = ({ data }) => {
     width = 450 - margin.left - margin.right,
     height = 480 - margin.top - margin.bottom;
 
+  const { nodes, links } = d3Sankey()
+    .nodeWidth(15)
+    .nodePadding(10)
+    .extent([[1, 1], [width - 1, height - 5]])(data);
 
+  // Color scale used
+  const color = d3.scaleOrdinal(d3.schemePastel1);
 
-  useEffect(() => {
-    const svg = select(svgRef.current)
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+  const colorScale = d3
+    .scaleLinear()
+    .domain([0, nodes.length])
+    .range([0, 1]);
 
-    var sankey = d3Sankey()
-      .nodeWidth(36)
-      .nodePadding(290)
-      .size([width, height])
-      .nodes(data.nodes)
-      .links(data.links)
-
-
-    var link = svg.append("g")
-      .selectAll(".link")
-      .data(data.links)
-      .enter()
-      .append("path")
-      .attr("class", "link")
-
-    var node = svg.append("g")
-      .selectAll(".node")
-      .data(data.nodes)
-      .enter()
-      .append("g")
-      .attr("class", "node")
-    // .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-
-  }, [data])
+    console.log(nodes)
 
   return (
     <div ref={wrapperRef} style={{ marginBottom: '2rem' }} align="center">
       <h3> Sankey Diagram </h3>
 
       <svg id="svg" style={{ width: 960, height: 500 }} ref={svgRef}>
+
+      <g style={{ mixBlendMode: "multiply" }}>
+        {nodes.map((node, i) => (
+          <SankeyNode
+            {...node}
+            key={i}
+            color={color(colorScale(i))}
+            key={node.name}
+          />
+        ))}
+
+        {links.map((link, i) => (
+          <SankeyLink
+            link={link}
+            key={i}
+            color={color(colorScale(link.source.index))}
+          />
+        ))}
+      </g>
       </svg>
 
     </div>
